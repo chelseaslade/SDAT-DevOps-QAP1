88d635f2ce2c132ec5f4bd40c9558ab7ab7905ab
Chelsea Slade <chelsea.mayne@keyin.com>
2025-01-29 14:42:07 -0330

    Add and subtract from balance unit tests - assertEquals

--- src/test/java/org/keyin/UnitTests.java
+++ src/test/java/org/keyin/UnitTests.java
@@ -4,4 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UnitTests {
+    @Test
+    public void addDepositToBalance() {
+        BankUser testUser = new BankUser("Test", "User");
+        testUser.depositFunds(100);
+        Assertions.assertEquals(100, testUser.getBalance());
+    }
+
+    @Test
+    public void subtractWithdrawFromBalance() {
+        BankUser testUser = new BankUser("Test", "User");
+        testUser.depositFunds(100);
+        testUser.withdrawFunds(100);
+        Assertions.assertEquals(0, testUser.getBalance());
+    }
 }
+
